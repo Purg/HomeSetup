eab01268677d54d578fb1e702a72e3ac9c056f8e
Paul Tunison <paul.tunison@kitware.com>
2017-07-19 20:16:06 -0400

    Fix task project list script
    Was not catching white space discrepencies as well as when the "Project"
    word showed up in other spots of a line.

--- bin/task.prev_project_names.sh
+++ bin/task.prev_project_names.sh
@@ -3,4 +3,4 @@
 # Simple script for showing the names of projects that have been entered for
 # any task in history.
 #
-task information | grep Project | sort | uniq
+task information | awk '{print $1, $2}' | egrep '^Project' | sort | uniq
