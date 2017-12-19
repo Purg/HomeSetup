f200b1122a02a83b494081556f3ea541ce7ad3c3
Paul Tunison <paul.tunison@kitware.com>
2017-12-19 11:09:01 -0500

    fix typo: rwd -> rwx

--- bin/setup_shared_directory.sh
+++ bin/setup_shared_directory.sh
@@ -13,6 +13,6 @@ chown -R root:$2 "${dirpath}"
 echo "Adding sticky bit to directories"
 find "${dirpath}" -type d | xargs -n 16 chmod g+s
 echo "Adding ACL rules"
-# Defaut group ownership to rwd for dirs (rw- for files)
+# Defaut group ownership to rwx for dirs (rw- for files)
 # and no permissions for other.
 setfacl -R -m g:${groupname}:7,o:0 -dm g:${groupname}:7,o:0 "${dirname}"
